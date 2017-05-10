de280889163cbf4e4be8ad67d45db45859fe8173
Aashirya <ak1107@wildcats.unh.edu>
2017-05-10 17:15:35 -0500

    adding port 8080

--- unh698.py
+++ unh698.py
@@ -10,4 +10,4 @@ def new_page():
     return render_template('new_page.html')
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True,host='0.0.0.0', port=8080)
